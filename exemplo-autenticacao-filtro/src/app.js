cc7441c51aef27894da35510a242b76dbb0b3d0d
Sidney Sousa <sidney.sousa@ifms.edu.br>
2020-06-19 09:29:43 -0400

    Adicionando projeto exemplo-autenticacao-filtro

---
+++ exemplo-autenticacao-filtro/src/app.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+
+import rotasPaginas from './routes/paginas';
+
+const app = express();
+
+/**
+ * Configuração das páginas
+ */
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+/**
+ * Configuração dos recursos estáticos
+ */
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * Configuração do Body Parser
+ */
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
+/**
+ * Rotas de view
+ */
+app.use('/', rotasPaginas);
+
+
+
+
+export default app;
---
+++ exemplo-autenticacao-filtro/src/models/Usuario.js
@@ -0,0 +1,8 @@
+export default class Usuario {
+
+    constructor(login, nome, senha) {
+        this.login = login;
+        this.nome = nome;
+        this.senha = senha;
+    }
+}
